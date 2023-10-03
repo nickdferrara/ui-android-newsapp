926288271fdad1f9ead3b1f1732ef8b0804273ff
nickdferrara <nick.d.ferrara@gmail.com>
2023-10-02 21:59:14 -0400

    Make new york times api

--- app/src/main/java/com/nickdferrara/ui_android_newsapp/data/remote/NewYorkTimesApi.kt
+++ app/src/main/java/com/nickdferrara/ui_android_newsapp/data/remote/NewYorkTimesApi.kt
@@ -1,5 +1,17 @@
 package com.nickdferrara.ui_android_newsapp.data.remote
 
-interface NewYorkTimesApi {
+import com.nickdferrara.ui_android_newsapp.data.remote.responses.ArticleList
+import retrofit2.http.GET
+import retrofit2.http.Query
 
+interface NewYorkTimesApi {
+    @GET("home.json/api-key=$API_KEY")
+    suspend fun getArticleList(
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int
+    ): ArticleList
+
+    companion object {
+        const val API_KEY = "*"
+    }
 }
