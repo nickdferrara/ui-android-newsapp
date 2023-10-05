7cf9d8fa37816cc0e97a6dd8d57da046f2a8b70d
nickdferrara <nick.d.ferrara@gmail.com>
2023-10-04 22:21:06 -0400

    Improve homepage layout

--- app/src/main/java/com/nickdferrara/ui_android_newsapp/screen/home/HomeScreen.kt
+++ app/src/main/java/com/nickdferrara/ui_android_newsapp/screen/home/HomeScreen.kt
@@ -1,11 +1,15 @@
 package com.nickdferrara.ui_android_newsapp.screen.home
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +20,7 @@ import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,14 +43,31 @@ fun HomeScreen(
     ) {
         LazyColumn() {
             items(articleList) {article ->
-                Column {
+                Column(modifier = Modifier.padding(top = 16.dp)) {
                     Text(
                         text = article.articleTitle,
                         color = Color.Black,
                         fontSize = 26.sp,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier.fillMaxWidth()
+                        textAlign = TextAlign.Start,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier
+                            .padding(start = 10.dp, end = 10.dp)
                     )
+
+                    Spacer(modifier = Modifier.height(8.dp))
+
+                    Text(
+                        text = article.articleSubTitle,
+                        color = Color.Gray,
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Start,
+                        fontWeight = FontWeight.Normal,
+                        modifier = Modifier
+                            .padding(start = 10.dp, end = 10.dp)
+
+                    )
+
+                    Spacer(modifier = Modifier.height(20.dp))
 
                     SubcomposeAsyncImage(
                         model = ImageRequest.Builder(LocalContext.current)
@@ -53,10 +75,11 @@ fun HomeScreen(
                             .crossfade(true)
                             .build(),
                         contentDescription = "${article.articleImage} image",
-
                         modifier = Modifier
-                            .fillMaxSize()
+                           
                     )
+
+                    Spacer(modifier = Modifier.height(20.dp))
                 }
             }
         }
